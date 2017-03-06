75fa0e18ac5c8e4093b212127c1680acedc46576
Rob <robflettcc@gmail.com>
2017-03-06 15:46:36 +0000

    Two passing tests

--- Movie.java
+++ Movie.java
@@ -9,6 +9,19 @@ public Movie(String name){
   this.cast = new Actor[9];
 }
 
+public String getName() {
+  return this.name;
+}
+
+public int castSize() {
+  int count = 0;
+  for (Actor actor : cast) {
+    if (actor != null) {
+      count = count++;
+    }
+  }
+  return count;
+}
 
 
 }
--- MovieTest.java
+++ MovieTest.java
@@ -5,5 +5,20 @@ public class MovieTest {
 
   Movie movie;
   Actor actor;
-  
+
+  @Before
+  public void before() {
+    movie = new Movie("Aliens");
+    actor = new Actor();
+  }
+
+  @Test
+  public void hasName() {
+    assertEquals("Aliens", movie.getName() );
+  }
+
+  @Test
+  public void castStartsEmpty(){
+    assertEquals(0, movie.castSize() );
+  }
 }
