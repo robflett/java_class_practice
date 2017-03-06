fb305334ca4e993ef44aef42e41cd2637d705cbd
Rob <robflettcc@gmail.com>
2017-03-06 15:34:05 +0000

    created classes and test file

---
+++ Movie.java
@@ -0,0 +1,14 @@
+class Movie {
+
+  private String name;
+  private Actor[] cast;
+
+
+public Movie(String name){
+  this.name = name;
+  this.cast = new Actor[9];
+}
+
+
+
+}
---
+++ MovieTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+
+public class MovieTest {
+
+  Movie movie;
+  Actor actor;
+  
+}
